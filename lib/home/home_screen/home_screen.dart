3049032eaafb62c5dcfd5099a7682de9cd53b42b
Md Arif Bhuiyan <arifhiossen@3499email.com>
2025-05-09 15:41:05 +0600

    second commit

--- lib/home/home_screen/home_screen.dart
+++ lib/home/home_screen/home_screen.dart
@@ -23,29 +23,32 @@ class _HomeScreenState extends State<HomeScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('User List')),
-      body: FutureBuilder<List<User>>(
-        future: _homeCon.fetchUsersDetails(), // from above
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.waiting) {
-            return const Center(child: CircularProgressIndicator());
-          } else if (snapshot.hasError) {
-            return Center(child: Text('Error: ${snapshot.error}'));
-          } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
-            return const Center(child: Text('No users found.'));
-          } else {
-            final users = snapshot.data!;
-            return ListView.builder(
-              itemCount: users.length,
-              itemBuilder: (context, index) {
-                final user = users[index];
-                return getItem(user, index,
-                    onBackToScreen: () {
-                      // loadData();
-                    });
-              },
-            );
-          }
-        },
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: FutureBuilder<List<User>>(
+          future: _homeCon.fetchUsersDetails(), // from above
+          builder: (context, snapshot) {
+            if (snapshot.connectionState == ConnectionState.waiting) {
+              return const Center(child: CircularProgressIndicator());
+            } else if (snapshot.hasError) {
+              return Center(child: Text('Error: ${snapshot.error}'));
+            } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+              return const Center(child: Text('No users found.'));
+            } else {
+              final users = snapshot.data!;
+              return ListView.builder(
+                itemCount: users.length,
+                itemBuilder: (context, index) {
+                  final user = users[index];
+                  return getItem(user, index,
+                      onBackToScreen: () {
+                        // loadData();
+                      });
+                },
+              );
+            }
+          },
+        ),
       ),
     );
   }
